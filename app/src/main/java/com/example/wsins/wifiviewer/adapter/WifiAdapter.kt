48cb747199213e42f498a34b19233f8813ee255c
WangSins <WangSins@126.com>
2019-04-07 17:50:39 +0800

    Optimizing adapter.

--- app/src/main/java/com/example/wsins/wifiviewer/adapter/WifiAdapter.kt
+++ app/src/main/java/com/example/wsins/wifiviewer/adapter/WifiAdapter.kt
@@ -5,47 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.TextView
 import com.example.wsins.wifiviewer.R
 
 import com.example.wsins.wifiviewer.info.WifiInfo
+import com.example.wsins.wifiviewer.viewholder.WifiViewHolder
 
 class WifiAdapter(val mContext: Context) : BaseAdapter() {
 
-    private var mWifiInfos: List<WifiInfo>? = null
+    lateinit var mWifiInfos: List<WifiInfo>
 
     fun setData(wifiInfos: List<WifiInfo>) {
         mWifiInfos = wifiInfos
     }
 
-    override fun getCount(): Int = mWifiInfos!!.size
+    override fun getCount(): Int = mWifiInfos.size
 
-    override fun getItem(position: Int): Any = mWifiInfos!![position]
+    override fun getItem(position: Int): Any = mWifiInfos[position]
 
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var mWifiViewHold: WifiViewHold
-        var mItemView: View
+        val mWifiViewHolder: WifiViewHolder
+        val mItemView: View
         if (convertView == null) {
-            mWifiViewHold = WifiViewHold()
+            mWifiViewHolder = WifiViewHolder()
             mItemView = LayoutInflater.from(mContext).inflate(R.layout.item_wifi, null)
-            mWifiViewHold.tv_wifi_name = mItemView.findViewById(R.id.tv_wifi_name)
-            mWifiViewHold.tv_wifi_pwd = mItemView.findViewById(R.id.tv_wifi_pwd)
-            mItemView.tag = mWifiViewHold
+            mWifiViewHolder.tv_wifi_name = mItemView.findViewById(R.id.tv_wifi_name)
+            mWifiViewHolder.tv_wifi_pwd = mItemView.findViewById(R.id.tv_wifi_pwd)
+            mItemView.tag = mWifiViewHolder
 
         } else {
             mItemView = convertView
-            mWifiViewHold = mItemView.tag as WifiViewHold
+            mWifiViewHolder = mItemView.tag as WifiViewHolder
         }
-        mWifiViewHold.tv_wifi_name.text = mWifiInfos!![position].ssid
-        mWifiViewHold.tv_wifi_pwd.text = mWifiInfos!![position].password
+        mWifiViewHolder.tv_wifi_name.text = mWifiInfos[position].ssid
+        mWifiViewHolder.tv_wifi_pwd.text = mWifiInfos[position].password
         return mItemView
     }
-
-    class WifiViewHold {
-        lateinit var tv_wifi_name: TextView
-        lateinit var tv_wifi_pwd: TextView
-    }
-
 }
---
+++ app/src/main/java/com/example/wsins/wifiviewer/viewholder/WifiViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.wsins.wifiviewer.viewholder
+
+import android.widget.TextView
+
+class WifiViewHolder {
+    lateinit var tv_wifi_name: TextView
+    lateinit var tv_wifi_pwd: TextView
+}
