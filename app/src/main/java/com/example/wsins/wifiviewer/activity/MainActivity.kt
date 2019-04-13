8b7e6b01de7cd5f131759d166a90d9161a606d4b
WangSins <WangSins@126.com>
2019-04-13 21:16:41 +0800

    Add root authorization detection.

--- app/src/main/java/com/example/wsins/wifiviewer/activity/MainActivity.kt
+++ app/src/main/java/com/example/wsins/wifiviewer/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.OrientationHelper
@@ -18,6 +19,7 @@ import com.example.wsins.wifiviewer.adapter.WifiAdapter
 import com.example.wsins.wifiviewer.info.WifiInfo
 import com.example.wsins.wifiviewer.utils.ClipBoardUtils
 import com.example.wsins.wifiviewer.utils.DensityUtils
+import com.example.wsins.wifiviewer.utils.RootUtils
 import com.example.wsins.wifiviewer.utils.WifiManage
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -29,9 +31,24 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.wsins.wifiviewer.R.layout.activity_main)
+        initRoot()
         getData()
         initView()
         initListener()
+    }
+
+    private fun initRoot() {
+        if (!RootUtils().checkRoot()) {
+            AlertDialog.Builder(this).run {
+                setTitle("Root权限检测")
+                setMessage("未获取Root权限。")
+                setCancelable(false)
+                setPositiveButton("退出") { _, _ ->
+                    this@MainActivity.finish()
+                }
+                show()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -70,7 +87,7 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
 
             override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
                 super.getItemOffsets(outRect, view, parent, state)
-                outRect.set(0, 0, 0, DensityUtils.dip2px(this@MainActivity,4.5f))
+                outRect.set(0, 0, 0, DensityUtils.dip2px(this@MainActivity, 4.5f))
             }
         })
 
@@ -84,12 +101,12 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
 
     override fun onItemClick(position: Int) {
         ClipBoardUtils().copyClipBoard(this, "wifipwd", mWifiInfos[position].password)
-        Snackbar.make(rv_wifi_list,"已复制密码 ${mWifiInfos[position].password} 到剪贴板。",Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(rv_wifi_list, "已复制密码 ${mWifiInfos[position].password} 到剪贴板。", Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onItemLongClick(position: Int) {
         ClipBoardUtils().copyClipBoard(this, "wifissidpwd", "SSID：" + mWifiInfos[position].ssid + "\n密码：" + mWifiInfos[position].password)
-        Snackbar.make(rv_wifi_list,"已复制 ${mWifiInfos[position].ssid} 的SSID和密码到剪贴板。",Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(rv_wifi_list, "已复制 ${mWifiInfos[position].ssid} 的SSID和密码到剪贴板。", Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onRefresh() {
@@ -98,7 +115,7 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
             mWifiAdapter.setData(mWifiInfos)
             mWifiAdapter.notifyDataSetChanged()
             srl_wifi_list.isRefreshing = false
-            Snackbar.make(rv_wifi_list,"刷新完成。",Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(rv_wifi_list, "刷新完成。", Snackbar.LENGTH_SHORT).show()
         }, 500)
     }
 
@@ -106,8 +123,7 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             if ((System.currentTimeMillis() - mExitTime) > 2000) {
-                Snackbar.make(rv_wifi_list,"再按一次退出程序。",Snackbar.LENGTH_SHORT).show()
-
+                Snackbar.make(rv_wifi_list, "再按一次退出程序。", Snackbar.LENGTH_SHORT).show()
                 mExitTime = System.currentTimeMillis()
             } else {
                 finish()
---
+++ app/src/main/java/com/example/wsins/wifiviewer/utils/RootUtils.kt
@@ -0,0 +1,62 @@
+package com.example.wsins.wifiviewer.utils
+
+import java.io.DataOutputStream
+import java.io.File
+import java.io.IOException
+
+/**
+ * Created by Sin on 2019/4/13
+ */
+class RootUtils {
+
+    val isRoot: Boolean
+        get() {
+            var bool = false
+
+            try {
+                bool = !(!File("/system/bin/su").exists() && !File("/system/xbin/su").exists())
+            } catch (e: Exception) {
+
+            }
+            return bool
+        }
+
+    private fun rumCmd(): Boolean {
+        var process: Process? = null
+        var os: DataOutputStream? = null
+        val rt: Int
+        try {
+            process = Runtime.getRuntime().exec("su")
+            os = DataOutputStream(process!!.outputStream)
+            os.writeBytes("system/bin/mount -o rw,remount -t rootfs /data" + "\n")
+            os.writeBytes("exit\n")
+            os.flush()
+            rt = process.waitFor()
+        } catch (e: Exception) {
+            return false
+        } finally {
+            if (os != null) {
+                try {
+                    os.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+            process?.destroy()
+        }
+        return rt == 0
+    }
+
+    fun checkRoot(): Boolean {
+        return try {
+            try {
+                rumCmd()
+            } catch (e: Exception) {
+                false
+            }
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
