c2b54e0db24563481b725dbcecfb7e27456f7683
WangSins <WangSins@126.com>
2020-10-12 00:47:50 +0800

    Change the layout of the front page and discard the sideslip menu.

--- app/src/main/java/com/eshen/wifiviewer/MainActivity.kt
+++ app/src/main/java/com/eshen/wifiviewer/MainActivity.kt
@@ -9,7 +9,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -21,29 +20,17 @@ import com.eshen.wifiviewer.presenter.WifiPresenter
 import com.eshen.wifiviewer.util.ActivityManager
 import com.eshen.wifiviewer.util.dp
 import com.eshen.wifiviewer.util.showSnackBar
-import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.empty_layout.*
-import kotlinx.android.synthetic.main.nav_header.view.*
 
-class MainActivity() : BaseActivity(), WifiContract.IWifiView,
-        SwipeRefreshLayout.OnRefreshListener, NavigationView.OnNavigationItemSelectedListener {
+class MainActivity() : BaseActivity(), WifiContract.IWifiView, SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var mWifiAdapter: WifiAdapter
     private lateinit var mPresenter: WifiPresenter
     private var mExitTime: Long = 0
 
     override fun getLayoutResID(): Int = R.layout.activity_main
-
-    override fun initActionBar() {
-        setSupportActionBar(toolbar)
-        supportActionBar?.run {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeButtonEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
-        }
-    }
 
     override fun initView() {
         mWifiAdapter = WifiAdapter()
@@ -75,7 +62,6 @@ class MainActivity() : BaseActivity(), WifiContract.IWifiView,
 
     override fun initEvent() {
         wifi_list_srl.setOnRefreshListener(this)
-        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun release() {
@@ -89,39 +75,55 @@ class MainActivity() : BaseActivity(), WifiContract.IWifiView,
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            android.R.id.home -> drawer_layout.openDrawer(GravityCompat.START)
             R.id.item_setting -> {
                 startActivity(Intent().apply {
                     action = "android.net.wifi.PICK_WIFI_NETWORK"
                 })
+            }
+            R.id.item_share -> {
+                startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_information))
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }, getString(R.string.share_app)))
+            }
+            R.id.item_about -> {
+                with(AlertDialog.Builder(this@MainActivity)) {
+                    setTitle(context.getString(R.string.warm_prompt))
+                    setMessage(context.getString(R.string.about_prompt_information))
+                    setNegativeButton(context.getString(R.string.open_source)) { _: DialogInterface?, _: Int ->
+                        startActivity(Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://github.com/WangSins/WifiViewer")
+                        ))
+                    }
+                    setPositiveButton(context.getString(R.string.close)) { dialogInterface: DialogInterface, _: Int ->
+                        dialogInterface.dismiss()
+                    }
+                    show()
+                }.run {
+                    setCanceledOnTouchOutside(false)
+                }
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawers()
-            return true
-        } else {
-            if (keyCode == KeyEvent.KEYCODE_BACK) {
-                if ((System.currentTimeMillis() - mExitTime) > 2000) {
-                    wifi_list_rv.showSnackBar(getString(R.string.press_exit_again), Snackbar.LENGTH_SHORT)
-                    mExitTime = System.currentTimeMillis()
-                } else {
-                    finish()
-                }
-                return true
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - mExitTime) > 2000) {
+                wifi_list_rv.showSnackBar(getString(R.string.press_exit_again), Snackbar.LENGTH_SHORT)
+                mExitTime = System.currentTimeMillis()
+            } else {
+                finish()
             }
+            return true
         }
         return super.onKeyDown(keyCode, event)
     }
 
     override fun loadSuccess(loadStyle: Int, response: MutableList<WifiBean>) {
-        nav_view.run {
-            getHeaderView(0).app_name.text = getString(R.string.app_name)
-            getHeaderView(0).wifi_count.text = String.format(resources.getString(R.string.a_total_of_n_wifi_messages), response.size)
-        }
+        supportActionBar?.subtitle = String.format(resources.getString(R.string.a_total_of_n_wifi_messages), response.size)
         mWifiAdapter.setData(response)
         wifi_list_srl.isRefreshing = false
         empty_layout.visibility = View.GONE
@@ -151,37 +153,5 @@ class MainActivity() : BaseActivity(), WifiContract.IWifiView,
 
     override fun onRefresh() {
         mPresenter.readData(DATA_REFRESH)
-    }
-
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        when (p0.itemId) {
-            R.id.nav_share -> {
-                startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
-                    type = "text/plain"
-                    putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_information))
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                }, getString(R.string.share_app)))
-            }
-            R.id.nav_about -> {
-                with(AlertDialog.Builder(this@MainActivity)) {
-                    setTitle(context.getString(R.string.warm_prompt))
-                    setMessage(context.getString(R.string.about_prompt_information))
-                    setNegativeButton(context.getString(R.string.open_source)) { _: DialogInterface?, _: Int ->
-                        startActivity(Intent(
-                                Intent.ACTION_VIEW,
-                                Uri.parse("https://github.com/WangSins/WifiViewer")
-                        ))
-                    }
-                    setPositiveButton(context.getString(R.string.close)) { dialogInterface: DialogInterface, _: Int ->
-                        dialogInterface.dismiss()
-                    }
-                    show()
-                }.run {
-                    setCanceledOnTouchOutside(false)
-                }
-            }
-        }
-        drawer_layout.closeDrawers()
-        return true
     }
 }
